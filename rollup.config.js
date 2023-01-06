6464f7dd5ee1d60f22a58a42eec1f673c01d494c
oscelest <oscelest@gmail.com>
2023-01-06 15:25:19 +0100

    Updated to v0.2.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,55 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
-import {terser} from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-
-const pkg = require("./package.json");
+import Resolve from "@rollup/plugin-node-resolve";
+import CommonJS from "@rollup/plugin-commonjs";
+import TypeScript from "@rollup/plugin-typescript";
+import PostCSS from "rollup-plugin-postcss";
+import DTS from "rollup-plugin-dts";
+import Terser from "@rollup/plugin-terser";
+import PeerDepsExternal from "rollup-plugin-peer-deps-external";
+import Package from "./package.json" assert {type: "json"};
 
 export default [
   {
     input:   "src/index.ts",
     output:  [
       {
-        file:      pkg.main,
-        format:    "cjs",
-        sourcemap: true,
+        file:   "dist/cjs/index.js",
+        format: "cjs",
       },
       {
-        file:      pkg.module,
-        format:    "esm",
-        sourcemap: true,
+        file:   "dist/esm/index.js",
+        format: "esm",
       },
       {
-        file:      pkg.umd,
-        format:    "umd",
-        name:      pkg.name.replace(/^@noxy\//, "").split("-").map(v => v.charAt(0).toUpperCase() + v.slice(1)).join(""),
-        sourcemap: true,
-        globals:   {
+        name:    Package.name.replace(/^@noxy\//, ""),
+        file:    "dist/umd/index.js",
+        format:  "umd",
+        globals: {
           "react": "React",
         },
       },
     ],
     plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({tsconfig: "./tsconfig.json"}),
-      postcss(),
-      terser(),
+      PeerDepsExternal(), // Ensure peer-dependencies are not included in the bundle.
+      Resolve(),          // Resolve external libraries and adds them to the bundle.
+      CommonJS(),
+      TypeScript({tsconfig: "./tsconfig.json"}),
+      PostCSS(),
+      Terser({sourceMap: true}),
     ],
   },
   {
     input:    "dist/esm/types/index.d.ts",
-    output:   [{file: "dist/types/index.d.ts", format: "esm"}],
-    plugins:  [dts()],
-    external: [/\.css$/],
+    output:   [
+      {
+        file:   "dist/types/index.d.ts",
+        format: "esm",
+      },
+    ],
+    plugins:  [
+      DTS(),
+    ],
+    external: [
+      /\.css$/,
+    ],
   },
 ];
